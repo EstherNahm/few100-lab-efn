be1a0aaa20ee02910e0e2d9a1f8360f3bae3e3f0
Esther Nahm <49803818+EstherNahm@users.noreply.github.com>
2019-09-23 14:17:04 +0000

    working

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,71 @@
 import './styles.css';
 import { calculateTipAmount, calculateTotalBill } from './math';
 
-const billamount = document.getElementById('billamount') as HTMLInputElement;
-const billamount1 = document.getElementById('billamount1');
-const tipamount = document.getElementById('tipamount');
+const billamountinputcontrol = document.getElementById('billamountinput') as HTMLInputElement;
+const billamountdisplaycontrol = document.getElementById('billamountdisplay');
+const tippercentagecontrol = document.getElementById('tippercentage');
 let tipAmounts: number;
+tipAmounts = 0;
+let selectedtippercent: number;
+selectedtippercent = 0;
+let totalBillAmount: number;
+totalBillAmount = 0;
 
 const tip10 = document.getElementById('button1') as HTMLInputElement;
 const tip15 = document.getElementById('button2') as HTMLInputElement;
 const tip20 = document.getElementById('button3') as HTMLInputElement;
 
-const amountOfTip = document.getElementById('amountOfTip') as HTMLSpanElement;
+const amountOfTipControl = document.getElementById('amountOfTip') as HTMLSpanElement;
 const totalBill = document.getElementById('totalBill') as HTMLSpanElement;
 
 tip10.addEventListener('click', function () {
     tip10.disabled = true;
-    tip10.disabled = false;
-    tip10.disabled = false;
-    tipAmounts = calculateTipAmount(billamount.valueAsNumber, .10);
-
-    amountOfTip.innerText = tipAmounts.toString();
-    tipamount.innerText = '10%';
+    tip15.disabled = false;
+    tip20.disabled = false;
+    selectedtippercent = .10;
+    updateDisplay();
 });
 
 tip15.addEventListener('click', function () {
     tip15.disabled = true;
-    tip15.disabled = false;
-    tip15.disabled = false;
-    tipAmounts = calculateTipAmount(billamount.valueAsNumber, .15);
-
-    amountOfTip.innerText = tipAmounts.toString();
-    tipamount.innerText = '15%';
+    tip20.disabled = false;
+    tip10.disabled = false;
+    selectedtippercent = .15;
+    updateDisplay();
 });
 
 tip20.addEventListener('click', function () {
     tip20.disabled = true;
-    tip20.disabled = false;
-    tip20.disabled = false;
-    tipAmounts = calculateTipAmount(billamount.valueAsNumber, .20);
-
-    amountOfTip.innerText = tipAmounts.toString();
-    tipamount.innerText = '20%';
+    tip15.disabled = false;
+    tip10.disabled = false;
+    selectedtippercent = .20;
+    updateDisplay();
 });
 
 // when user types, add event listener to that event
-billamount.addEventListener('keyup', () => {
+billamountinputcontrol.addEventListener('keyup', () => {
     // add the listener to the event that's actually changing
     // read the value out of text box
     // convert into number for calculate function
     // print result into totalBill textbox
     // turn the innertext of totalbill to a string
-    if (billamount.valueAsNumber < 0) {
-        billamount.classList.add('error');
+
+    if (billamountinputcontrol.valueAsNumber < 0) {
+        billamountinputcontrol.classList.add('error');
     } else {
-        billamount1.innerText = billamount.valueAsNumber.toString();
-        const totalSum = calculateTotalBill(billamount.valueAsNumber, tipAmounts);
-        totalBill.innerText = totalSum.toString();
+        updateDisplay();
     }
 });
 
+function updateDisplay() {
+    tipAmounts = calculateTipAmount(billamountinputcontrol.valueAsNumber, selectedtippercent);
+    const billamount = billamountinputcontrol.valueAsNumber;
+    const totalSum = calculateTotalBill(billamount, tipAmounts);
+    totalBill.innerText = totalSum.toString();
+    amountOfTipControl.innerText = tipAmounts.toString();
+    tippercentagecontrol.innerText = `${(selectedtippercent * 100)}%`;
+    billamountdisplaycontrol.innerText = billamount.toString();
+}
 
-
-
+// turn all controls inner text into empty strings
+//
